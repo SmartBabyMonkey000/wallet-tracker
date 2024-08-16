9fe0abcff3760c045d118a57af0dd7f9e72482a2
smartbabymonkey000 <smartbabymonkey000@gmail.com>
2024-08-16 21:08:01 +0000

    add the funcction to invest tol

--- monitor.js
+++ monitor.js
@@ -10,6 +10,7 @@ dotenv.config();
 
 const RPC_URL = process.env.RPC_URL;
 const TARGET_WALLET_LIST = process.env.TARGET_WALLET_LIST;
+const SOL_DIFF = process.env.SOL_DIFF;
 
 let alertState = false;
 
@@ -56,6 +57,7 @@ async function getTokenMetaData(connection, tokenMint) {
 }
 
 function getRandomConnection(conList) {
+    //console.log(Math.floor(Math.random() * conList.length))
     return conList[Math.floor(Math.random() * conList.length)];
 }
 
@@ -83,50 +85,67 @@ async function monitorWallet() {
     });
 
     let walletTokenLists = walletAddresses.reduce((acc, addr) => {
-        acc[addr] = { prevTokenList: [], curTokenList: []};
+        acc[addr] = { prevTokenList: [], curTokenList: [], solBalance : 0};
         return acc;
     }, {});
 
     while (true) {
-        for (const walletAddr of walletAddresses) {
-            const connection = getRandomConnection(conList);
-            const walletPublicKey = new PublicKey(walletAddr);
-            const filter = { programId: TOKEN_PROGRAM_ID };
-            const tokenAccounts = await connection.getTokenAccountsByOwner(walletPublicKey, filter);
-        
-            //console.log(`Token Accounts for wallet ${walletAddr}:`, tokenAccounts.value.length);
-        
-            walletTokenLists[walletAddr].curTokenList = [];
-        
-            for (const tokenAccount of tokenAccounts.value) {
-                const accountData = AccountLayout.decode(tokenAccount.account.data);
-                const tokenPublicKey = new PublicKey(accountData.mint);
-                const tokenBalance = await connection.getTokenAccountBalance(tokenAccount.pubkey);
-        
-                //console.log(`Token: ${tokenPublicKey.toBase58()}, Balance: ${tokenBalance.value.uiAmount}`);
-        
-                walletTokenLists[walletAddr].curTokenList.push(tokenPublicKey.toString());
-            }
-        
-            const result = arrayDifference(walletTokenLists[walletAddr].curTokenList, walletTokenLists[walletAddr].prevTokenList);
-            if (result.length > 0 && walletTokenLists[walletAddr].prevTokenList.length > 0) {
-                console.log(`New Token Detected for wallet ${walletAddr}:`, result[0]);
-                // Handle new token detection (e.g., notify user)
-                const tokenMint = new PublicKey(result[0]);
-                const token = await getTokenMetaData(connection, tokenMint);
-                token.wallet = walletAddr;
-                console.log(token.name, token.symbol);
-                log_token_detection(token);
-            }
+        try {
+            for (const walletAddr of walletAddresses) {
+                const connection = getRandomConnection(conList);
+                const walletPublicKey = new PublicKey(walletAddr);
+                const filter = { programId: TOKEN_PROGRAM_ID };
+                const tokenAccounts = await connection.getTokenAccountsByOwner(walletPublicKey, filter);
+                const curSolBalance = await connection.getBalance(walletPublicKey) / LAMPORTS_PER_SOL;
+                const prevSolBalance = walletTokenLists[walletAddr].solBalance;
             
-            walletTokenLists[walletAddr].prevTokenList = [...walletTokenLists[walletAddr].curTokenList];
-        }
+                //console.log(`Token Accounts for wallet ${walletAddr}:`, tokenAccounts.value.length);
+            
+                walletTokenLists[walletAddr].curTokenList = [];
 
-        // Sleep for a specified interval before the next check
-        await sleep(1000);  // 10 seconds
+                if (prevSolBalance == 0) {
+                    walletTokenLists[walletAddr].solBalance = curSolBalance;
+                } else if ((curSolBalance - prevSolBalance) >= SOL_DIFF) {
+                    let wallet = null;
+                    wallet.address = walletAddr;
+                    wallet.invest = (curSolBalance - prevSolBalance);
+                    log_invest_detection(wallet);
+                }
+
+                walletTokenLists[walletAddr].solBalance = curSolBalance;
+            
+                for (const tokenAccount of tokenAccounts.value) {
+                    const accountData = AccountLayout.decode(tokenAccount.account.data);
+                    const tokenPublicKey = new PublicKey(accountData.mint);
+                    //const tokenBalance = await connection.getTokenAccountBalance(tokenAccount.pubkey);
+            
+                    //console.log(`Token: ${tokenPublicKey.toBase58()}, Balance: ${tokenBalance.value.uiAmount}`);
+            
+                    walletTokenLists[walletAddr].curTokenList.push(tokenPublicKey.toString());
+                }
+            
+                const result = arrayDifference(walletTokenLists[walletAddr].curTokenList, walletTokenLists[walletAddr].prevTokenList);
+                if (result.length > 0 && walletTokenLists[walletAddr].prevTokenList.length > 0) {
+                    console.log(`New Token Detected for wallet ${walletAddr}:`, result[0]);
+                    // Handle new token detection (e.g., notify user)
+                    const tokenMint = new PublicKey(result[0]);
+                    const token = await getTokenMetaData(connection, tokenMint);
+                    token.wallet = walletAddr;
+                    console.log(token.name, token.symbol);
+                    log_token_detection(token);
+                }
+                
+                walletTokenLists[walletAddr].prevTokenList = [...walletTokenLists[walletAddr].curTokenList];
+            }
+    
+            // Sleep for a specified interval before the next check
+            await sleep(1000);  // 10 seconds
+
+        } catch (err) {
+            console.log(err);
+        }
     }
 }
-
 
 function log_token_detection(token) {
   try {
@@ -162,6 +181,30 @@ ${token.wallet}
     console.log(e);
   }
 }
+
+function log_invest_detection(wallet) {
+    try {
+      const message = `🚀 Invest SOL
+  
+  📢 Status
+   INVEST
+    
+  🏆 Wallet
+  ${wallet.address}
+
+  💰 SOL
+  ${wallet.invest}`;
+  
+      if (alertState) {
+        alertMessage(message);
+        console.log(message);
+      }      
+  
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  
 
 module.exports = { initBot, monitorWallet, setState }
 
